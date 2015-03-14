f9c11f77dd5367661d46ec1b360ca1bf76d06d6b
hongyusu <hongyu.su.uh@gmail.com>
2015-03-14 10:09:49 +0200

    course slides

---
+++ Presentations/T61_6080_special_course_in_bioinformatics2/autorun.sh
@@ -0,0 +1,18 @@
+
+
+
+pdflatex example
+bibtex example
+bibtex example
+pdflatex example
+
+
+
+# rm example.aux
+# rm example.bbl
+# rm example.blg
+# rm example.log
+# rm example.nav
+# rm example.out
+# rm example.snm
+# rm example.toc
