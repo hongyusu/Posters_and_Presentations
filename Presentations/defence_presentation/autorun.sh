8ce26eb6a269ffd0485244217ca91484c2276874
hongyusu <hongyu.su.uh@gmail.com>
2015-03-24 15:34:26 +0200

    auto upload

--- Presentations/defence_presentation/autorun.sh
+++ Presentations/defence_presentation/autorun.sh
@@ -8,14 +8,14 @@ pdflatex example
 
 
 
- rm example.aux
- rm example.bbl
- rm example.blg
- rm example.log
- rm example.nav
- rm example.out
- rm example.snm
- rm example.toc
+ # rm example.aux
+ # rm example.bbl
+ # rm example.blg
+ # rm example.log
+ # rm example.nav
+ # rm example.out
+ # rm example.snm
+ # rm example.toc
 
 
 git commit -m'auto upload' .
