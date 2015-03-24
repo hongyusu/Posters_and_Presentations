342665614ba769bcc9efb2b68238ec9de22d9676
hongyusu <hongyu.su.uh@gmail.com>
2015-03-24 09:53:57 +0200

    commit defence presentationsl .

---
+++ Presentations/defence_presentation/autorun.sh
@@ -0,0 +1,21 @@
+
+
+
+pdflatex example
+bibtex example
+bibtex example
+pdflatex example
+
+
+
+ rm example.aux
+ rm example.bbl
+ rm example.blg
+ rm example.log
+ rm example.nav
+ rm example.out
+ rm example.snm
+ rm example.toc
+
+
+git commit -m'auto upload' .
