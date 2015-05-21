109eba8274aba47ef74f46d920536abfd97f5b04
hongyusu <hongyu.su.uh@gmail.com>
2015-05-21 11:55:15 +0300

    finish duality gap, review the rest

--- Presentations/group_meeting_20150522/auto_compile_update.sh
+++ Presentations/group_meeting_20150522/auto_compile_update.sh
@@ -17,6 +17,8 @@ rm $name.snm
 rm $name.nav
 rm $name.out
 
+mv example.pdf group_meeting_RTA_With_Projected_Newton.pdf
 
+# update GitHub
 git commit -m'automatic commit' .
 git push -u origin master
